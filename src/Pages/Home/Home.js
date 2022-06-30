1090e14214178882c8b91b518c9817d83308b974
Probal Ranjan Paul <probal.ranjan1198@gmail.com>
2022-07-01 01:19:43 +0600

    added all tasks in home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,13 +2,16 @@ import { faListCheck, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Form } from 'react-bootstrap';
+import useTasks from '../../hooks/useTasks/usetasks';
 import './Home.css';
 
 const Home = () => {
 
+    const [tasks] = useTasks();
+
     return (
         <div className='container page-container px-4'>
-            <div className='single-section'>
+            <div className='single-section my-auto'>
                 <h3 style={{ color: "#0B204C", fontWeight: "600" }}><FontAwesomeIcon icon={faPlus} className="pe-2" />Add Task</h3>
                 <Form className='form-style'>
                     <Form.Group controlId="task">
@@ -21,7 +24,14 @@ const Home = () => {
                 </Form>
             </div>
             <div className='single-section' style={{ backgroundColor: "#eaf9f5" }}>
-                <h3 style={{ color: "#0B204C", fontWeight: "600" }}><FontAwesomeIcon icon={faListCheck} className="pe-2" />Tasks List</h3>
+                <h3 style={{ color: "#0B204C", fontWeight: "600" }}><FontAwesomeIcon icon={faListCheck} className="pe-2" />All Tasks List</h3>
+                <div className='task-list'>
+                    {
+                        tasks.map(task =>
+                            <h5 key={task._id}>{task.task}</h5>
+                        )
+                    }
+                </div>
             </div>
         </div>
     );
